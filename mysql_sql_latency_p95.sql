d0ff4310bd12f087d42d1f0cad333c0807fab6d8
dbaops <44490929+dbaops-com@users.noreply.github.com>
2020-06-03 14:12:48 +0800

    Update mysql_sql_latency_p95.sql

--- mysql_sql_latency_p95.sql
+++ mysql_sql_latency_p95.sql
@@ -1,4 +1,4 @@
-SET SESSION group_concat_max_len = 1048576;
+SET SESSION group_concat_max_len = 10485760;
 SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(ROUND(timer_wait/1000000) ORDER BY ROUND(timer_wait/1000000) DESC),',' , -ROUND(0.95*COUNT(CURRENT_SCHEMA))), ',', 1 ) AS P95 
 FROM performance_schema.events_statements_history 
 WHERE LEFT(DIGEST_TEXT,6) IN ('SELECT','INSERT','UPDATE','DELETE','COMMIT') 
