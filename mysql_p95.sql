17b9c205ddfb29779bc7f7cbb66f6687143c8b54
dbaops <44490929+dbaops-com@users.noreply.github.com>
2020-06-03 14:11:51 +0800

    Create mysql_p95.sql

---
+++ mysql_p95.sql
@@ -0,0 +1,6 @@
+SET SESSION group_concat_max_len = 1048576;
+SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(ROUND(timer_wait/1000000) ORDER BY ROUND(timer_wait/1000000) DESC),',' , -ROUND(0.95*COUNT(CURRENT_SCHEMA))), ',', 1 ) AS P95 
+FROM performance_schema.events_statements_history 
+WHERE LEFT(DIGEST_TEXT,6) IN ('SELECT','INSERT','UPDATE','DELETE','COMMIT') 
+AND CURRENT_SCHEMA NOT IN ('performance_schema','information_schema','mysql','sys') 
+AND CURRENT_SCHEMA IS NOT NULL;
